8fab34b57a989cd7bd5a26eef339a56e50e37b25
Victor Pimenta <victorpimenta.ti@gmail.com>
2020-11-08 16:01:00 -0300

    Projeto criado

---
+++ src/main/java/com/victorpimenta/cursonelioped/CursoneliopedApplication.java
@@ -0,0 +1,13 @@
+package com.victorpimenta.cursonelioped;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoneliopedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoneliopedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/victorpimenta/cursonelioped/CursoneliopedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victorpimenta.cursonelioped;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoneliopedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
