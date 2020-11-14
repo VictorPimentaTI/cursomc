d787dae303ee57d437d475a26c5235887639cbe3
Victor Pimenta <victorpimenta.ti@gmail.com>
2020-11-14 20:40:19 -0300

    Criando operação de instanciação

--- src/main/java/com/victorpimenta/cursonelioped/CursoneliopedApplication.java
+++ src/main/java/com/victorpimenta/cursonelioped/CursoneliopedApplication.java
@@ -1,13 +1,32 @@
 package com.victorpimenta.cursonelioped;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.victorpimenta.cursonelioped.domain.Categoria;
+import com.victorpimenta.cursonelioped.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CursoNelioPedApplication {
+public class CursoNelioPedApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursoNelioPedApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
